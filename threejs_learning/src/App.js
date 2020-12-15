6d95a44f6b2dc63ab6fb54a9625170fe3b5636d9
xiasong <xias@uchonor.com>
2020-12-15 10:40:24 +0800

    初始化

---
+++ threejs_learning/src/App.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-15 09:53:21
+ * @LastEditors: xiasong
+ * @LastEditTime: 2020-12-15 10:31:57
+ * @Description:
+ * @FilePath: \learn_threejs\threejs_learning\src\App.js
+ */
+import './App.css';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
