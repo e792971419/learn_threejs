30ff502acbdab94b9901cad978b64cd573172212
xiasong <xias@uchonor.com>
2020-12-15 11:45:36 +0800

    增加cross-env改变端口号

--- threejs_learning/src/App.js
+++ threejs_learning/src/App.js
@@ -2,19 +2,14 @@
  * @Author: your name
  * @Date: 2020-12-15 09:53:21
  * @LastEditors: xiasong
- * @LastEditTime: 2020-12-15 10:57:43
+ * @LastEditTime: 2020-12-15 11:32:58
  * @Description:
  * @FilePath: \learn_threejs\threejs_learning\src\App.js
  */
-import './App.css';
 import './global.less';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="test">less测试</h1>
-    </div>
-  );
+  return <div className="App">app</div>;
 }
 
 export default App;
