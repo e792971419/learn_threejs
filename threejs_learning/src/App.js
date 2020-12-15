b034e86722777ca51a9c2351d857b40978166fd2
xiasong <xias@uchonor.com>
2020-12-15 11:01:04 +0800

    增加less

--- threejs_learning/src/App.js
+++ threejs_learning/src/App.js
@@ -2,14 +2,19 @@
  * @Author: your name
  * @Date: 2020-12-15 09:53:21
  * @LastEditors: xiasong
- * @LastEditTime: 2020-12-15 10:31:57
+ * @LastEditTime: 2020-12-15 10:57:43
  * @Description:
  * @FilePath: \learn_threejs\threejs_learning\src\App.js
  */
 import './App.css';
+import './global.less';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="test">less测试</h1>
+    </div>
+  );
 }
 
 export default App;
